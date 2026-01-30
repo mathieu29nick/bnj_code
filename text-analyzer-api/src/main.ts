b256a31ec34002ccc219bff541c2c71c80837a1e
Nick Mathieu <rakotomanananick29@gmail.com>
2026-01-30 12:39:56 +0300

    some change

--- text-analyzer-api/src/main.ts
+++ text-analyzer-api/src/main.ts
@@ -1,18 +1,24 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
-export async function createNestServer() {
-  const server = express();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'https://text-anaylez-front.vercel.app',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
 
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
   );
-
-  app.enableCors({ origin: '*' });
-  await app.init();
-
-  return server;
+  await app.listen(process.env.PORT ?? 3100);
 }
+bootstrap();
